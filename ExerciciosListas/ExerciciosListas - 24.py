d836e08f4c5d28b263d3821fe589bfe83bb8e0ac
Jorge Henrique <jorgehenrrique@live.com>
2022-03-04 17:49:39 -0300

    Add files via upload

---
+++ ExerciciosListas/ExerciciosListas - 24.py
@@ -0,0 +1,18 @@
+# Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene os resultados em um vetor .
+# Depois, mostre quantas vezes cada valor foi conseguido. Dica: use um vetor de contadores(1-6) e uma função para gerar
+# numeros aleatórios, simulando os lançamentos dos dados.
+
+import random
+
+
+def aleatorio():
+    return random.randint(1, 6)
+
+
+lances = [0] * 6
+for i in range(100):
+    x = aleatorio()
+    lances[x - 1] = lances[x - 1] + 1
+
+for i in range(len(lances)):
+    print(f'O {i + 1}º lance saiu {lances[i]} vezes ')
