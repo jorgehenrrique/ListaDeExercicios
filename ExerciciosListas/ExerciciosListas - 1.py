b749fb59e9c49bb61d5f5d60e3e62b44bbc8138e
Jorge Henrique <jorgehenrrique@live.com>
2022-04-01 17:33:50 -0300

    Delete ExerciciosListas directory

--- ExerciciosListas/ExerciciosListas - 1.py
+++
@@ -1,7 +0,0 @@
-# 1. Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
-
-vet = []
-for i in range(5):
-    vet.append(int(input(f'Informe o {i + 1}º numero: ')))
-
-print(vet)
--- ExerciciosListas/ExerciciosListas - 2.py
+++
@@ -1,8 +0,0 @@
-# 2. Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
-
-vet = []
-for i in range(10):
-    vet.append(float(input(f'Informe o {i + 1}º numero: ')))
-
-vet.reverse()
-print(vet)
